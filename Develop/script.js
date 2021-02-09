d66d5881a42bdec04ed6dc8901f74a6f2944c44a
Tadhg McIntyre <mcintyretadhg@gmail.com>
2021-02-09 14:09:06 -0500

    added if statement to check character numbers and ask about special characters

--- Develop/script.js
+++ Develop/script.js
@@ -3,23 +3,82 @@
 
   checkbox (?) options that can be added to PassWord
   options are; lowercase, uppercase, numeric, and/or special characters
+
+  Array.prototype.copyWithin() - Copies a sequence of array elements within the array
+  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#instance_methods
 */
 
-// Assignment Code
+
+let alpha = ['a', 'b', 'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+let numeric = [0,1,2,3,4,5,6,7,8,9];
+let SpecChar = ['!','@','#','$','%','^','&','*','?','=','+'];
+
+
+// Add event listener to generate button
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function  writePassword(){
+  
+  let userUP;
+  let userLow;
+  let userNum;
+  let userSPCH;
 
-  passwordText.value = password;
+  //this creates prompt and will make var userchoice become a number that will be the password length
+  let userchoice = window.prompt('How many characters would you like to use? (8-128)'); 
+
+  //asks for number of characters, confirms it is the correct amount, if not then stop, otherwise go ahead
+  //and ask character types, then stores the character types as boolean values
+  if (!userchoice || userchoice < 8 || userchoice > 128) {
+    return;
+  }else {
+    userUP = window.confirm('Would you like to include Upper case letters?');
+    userLow = window.confirm('Would you like to include Lower case letters?');
+    userNum = window.confirm('Would you like to include Numbers?');
+    userSPCH = window.confirm('Would you like to include Special Characters?');
+  }
+
+  console.log(userUP + ' ' + userLow + ' ' + userNum + ' ' + userSPCH); //just for testing-remove for launch
+
+  
+
+  function generatePassword() {
+    console.log(userUP + ' ' + userLow + ' ' + userNum + ' ' + userSPCH); //just for testing-remove for launch
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+  }
+  
+  if (userUP || userLow || userNum || userSPCH) {
+    generatePassword()
+  }
+
+
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
+// Write password to the #password input
+//this will be the function that will put password on the page itself
+
+/*var password = generatePassword();
+var passwordText = document.querySelector("#password"); 
+passwordText.value = password;*/
+
+// generateBtn.addEventListener("click", writePassword);
 
 //character # requirments
 
