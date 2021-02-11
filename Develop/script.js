95acaf0730cb5949258bb6ec17bc92d2ea5b000a
Tadhg McIntyre <mcintyretadhg@gmail.com>
2021-02-11 13:45:50 -0500

    changed how function creates chooses characters to system that works, and more efficiently. after light testing, code seems to work correctly

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,12 @@
-/* -password options/requirements
-  minimum char-8   max-128
-
-  checkbox (?) options that can be added to PassWord
-  options are; lowercase, uppercase, numeric, and/or special characters
-
-  Array.prototype.copyWithin() - Copies a sequence of array elements within the array
-  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#instance_methods
-*/
-
-
-
-
 
 // Add event listener to generate button
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
+//function that holds everything for password creation
 function  writePassword(){
+  
   //creates variables for user choice of chatacters
-
   let userUP;
   let userLow;
   let userNum;
@@ -30,6 +18,7 @@ function  writePassword(){
   //asks for number of characters, confirms it is the correct amount, if not then stop, otherwise go ahead
   //and ask character types, then stores the character types as boolean values
   if (!userchoice || userchoice < 8 || userchoice > 128) {
+    window.alert('Your password must be at least 8 characters, and no more than 128 characters!')
     return;
   }else {
     userLow = window.confirm('Would you like to include Lower case letters?');
@@ -38,119 +27,79 @@ function  writePassword(){
     userSPCH = window.confirm('Would you like to include Special Characters?');
   }
 
-  console.log(userUP + ' ' + userLow + ' ' + userNum + ' ' + userSPCH); //just for testing-remove for launch
-
+  //list of characters
   var Lower= 'abcdefghijklmnopqrstuvwxyz';
   var Upper= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var Numeric= '0123456789';
   var SpecChar= '!@#$%^&*?=+';
 
-  let randLow
-    function getRanLow() {
-      if (userLow){
-        randLow = Lower.charAt(Math.floor(Math.random() * 26));
-      } else {
-        return;
-      }
-    }
+  //create variable 'choices' to concat character types into
+  var choices;
 
-    let randUP
-    function getRanUP() {
-      if (userUP){
-        randUP = Upper.charAt(Math.floor(Math.random() * 26));
-      } else {
-        return;
-      }
-    }
+//if all prompts are chosen
+  if (userLow && userUP && userNum && userSPCH) {
+    choices = Lower.concat(Upper, Numeric, SpecChar)
+  }
+  
+  //ifs for only 3 prompts selected
+   else if (userLow && userUP && userNum) {
+    choices = Lower.concat(Upper, Numeric)
+  } else if (userLow && userUP && userSPCH) {
+    choices = Lower.concat(Upper, SpecChar)
+  } else if (userLow && userNum && userSPCH) {
+    choices = Lower.concat(Numeric, SpecChar)
+  }else if (userUP && userNum && userSPCH) {
+    choices = Upper.concat(Numeric, SpecChar)
+  }
 
-    let randNum
-    function getRanNum() {
-      if (userNum){
-        randNum = Numeric.charAt(Math.floor(Math.random() * 10));
-      } else {
-        return;
-      }
-    }
-    
-    let randSPCH
-    function getRanSPCH() {
-      if (userSPCH){
-        randSPCH = SpecChar.charAt(Math.floor(Math.random() * 12));
-      } else {
-        return;
-      }
-    }
+  //begin ifs for 2 options selected
+  else if (userLow && userUP) {
+    choices = Lower.concat(Upper)
+  }else if (userLow && userNum) {
+    choices = Lower.concat(Numeric)
+  }else if (userLow && userSPCH) {
+    choices = Lower.concat(SpecChar)
+  }else if (userNum && userUP) {
+    choices = Numeric.concat(Upper)
+  }else if (userSPCH && userUP) {
+    choices = SpecChar.concat(Upper)
+  }else if (userNum && userSPCH) {
+    choices = Numeric.concat(SpecChar)
+  }
 
+  //ifs for if only 1 option is chosen
+  else if (userLow){
+    choices = Lower
+  }else if (userUP){
+    choices = Upper
+  }else if (userNum){
+    choices = Numeric
+  }else if (userSPCH){
+    choices = SpecChar
+  };
 
     //loop creating password until userchoice is matched in value to password.length
-    function generatePassword() {
-      console.log(userUP + ' ' + userLow + ' ' + userNum + ' ' + userSPCH); //just for testing-remove for launch
-    
-    
-      for ( i = 0; i < userchoice; i++) { //this will run until 'i' matches the length of userchoice
-            getRanLow();
-            getRanUP();
-            getRanNum();
-            getRanSPCH();
-            CharSet = [randLow, randUP, randNum, randSPCH];
-            Char = CharSet[(Math.floor(Math.random() * 4))];
-            password += Char;
-          }
+  function generatePassword() {
 
+    password = '';
 
-    //let CharSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*?=+"
-    /*
-    function generatePassword() {
-    var length = 8,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-        retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
+    for ( i = 0; i < userchoice; i++) { //this will run until 'i' matches the length of userchoice
+      Char = choices[(Math.floor(Math.random() * choices.length))];
+      password += Char;
     }
-    return retVal;
-    }
-    what if we go get random characters for each type then choose randomly which to use?
-    */
-
-    
-
-    //need to account for undefined outcomes of above
-
-    //HERE BEGINS THE SELECTIONS & ADDING THOSE SELECTIONS TO PASSWORD
-    //put each type choice into an array, then choose from the array for each character
-
-  getRanLow()
-  getRanUP()
-  getRanNum()
-  getRanSPCH()
-  console.log(randUP + ' ' + randLow + ' ' + randNum + ' ' + randSPCH); //just for testing-remove for launch
 
   }//end of generatePassword()
   
   if (userUP || userLow || userNum || userSPCH) {
     generatePassword()
   } else {
+    window.alert("You must select at least one character type to create password!")
     return;
   }
 
+  // Write password to the #password input
+  //this will be the function that will put password on the page itself
   var passwordText = document.querySelector("#password"); 
   passwordText.value = password;
-  placePW = document.createElement("HTMLTextAreaElement")
-  placePW.textContent = pas
-
-
+  
 }//end of writePassword()
-
-
-// Write password to the #password input
-//this will be the function that will put password on the page itself
-
-/*var password = generatePassword();
-var passwordText = document.querySelector("#password"); 
-passwordText.value = password;*/
-
-// generateBtn.addEventListener("click", writePassword);
-
-//character # requirments
-
-//password strength options; lower/upper case, numbers, special characters
